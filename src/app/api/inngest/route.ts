1295bdc8dc223f6448dc3b05784b83cb62b2d25b
0Armaan025 <armaan33000@gmail.com>
2026-01-08 18:52:57 +0530

    04: bg jobs

---
+++ src/app/api/inngest/route.ts
@@ -0,0 +1,11 @@
+import { serve } from "inngest/next";
+import { inngest } from "@/ingest/client";
+import { demoGenerate } from "@/ingest/functions";
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    demoGenerate,
+  ],
+});
